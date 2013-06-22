3f6d8d5ddb280a495f1219f928f90cf253d876c6
Francisco Rojas <frojas@groupon.com>
2013-06-22 17:04:00 -0400

    Add Exercise 6.4.[1-4]

---
+++ src/fp_oo/recursion.clj
@@ -0,0 +1,40 @@
+(ns fp-oo.recursion
+  (:gen-class))
+
+;; Exercise 6.4.1
+
+(def factorial
+  (fn [n]
+    (if (<= n 1)
+      1
+    (* n (factorial (dec n))))))
+
+;;(prn (factorial 5))
+
+;;Exercise 6.4.2
+(def factorial-2
+  (fn [n so-far]
+    (if (<= n 1)
+     so-far
+    (factorial-2 (dec n) (* n so-far)))))
+
+;;Exercise 6.4.3
+(def recursive-function
+  (fn [numbers so-far]
+    (if (empty? numbers)
+      so-far
+    (recursive-function (rest numbers) (+ so-far (first numbers))))))
+
+
+(def recursive-function-2
+  (fn [numbers so-far]
+    (if (empty? numbers)
+      so-far
+    (recursive-function-2 (rest numbers) (* so-far (first numbers))))))
+
+
+(def reduce-function
+  (fn [combiner numbers so-far]
+    (if (empty? numbers)
+      so-far
+    (reduce-function combiner (rest numbers) (combiner so-far (first numbers))))))
---
+++ test/fp_oo/recursion_test.clj
@@ -0,0 +1,30 @@
+(ns fp-oo.recursion-test
+  (:require [clojure.test :refer :all]
+            [fp-oo.recursion :refer :all]))
+
+
+(deftest a-factorial-test
+  (testing "factorial with the first pattern"
+    (is (= (factorial 5) 120))
+    (is (= (factorial 0) 1))
+    (is (= (factorial 1) 1))))
+
+(deftest a-factorial-2-test
+  (testing "factorial with the second pattern"
+    (is (= (factorial-2 5 1) 120))
+    (is (= (factorial-2 0 1) 1))
+    (is (= (factorial-2 1 1) 1))))
+
+(deftest a-test-for-recursive-function
+  (testing "recursive function to sum secuences"
+    (is (= (recursive-function [1 2 3 4] 0) 10))))
+
+(deftest a-test-for-recursive-function-2
+  (testing "recursive function to multiply secuences"
+    (is (= (recursive-function-2 [1 2 3 4] 1) 24))))
+
+(deftest a-test-for-reduce-function
+  (testing "recursive function to reduce secuences "
+    (is (= (reduce-function * [1 2 3 4] 1) 24))
+    (is (= (reduce-function + [1 2 3 4] 0) 10))))
+
